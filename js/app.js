bcc46edd11b6c90aeffd69bb6bad40e8925927e8
pondzapkw123 <konlawateiei@gmail.com>
2020-09-22 23:47:40 +0700

    add detail movie

---
+++ js/app.js
@@ -0,0 +1,17 @@
+document.addEventListener('init', function(event) {
+    var page = event.target;
+  
+    if (page.id === 'homemovie') {
+      page.querySelector('#pic1').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('../views/movie1.html')
+    }
+    page.querySelector('#pic2').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('../views/movie2.html')
+    }
+    page.querySelector('#pic3').onclick = function () {
+        document.querySelector('#myNavigator').pushPage('../views/movie3.html')
+    }
+    } else if (page.id === 'page2') {
+      page.querySelector('ons-toolbar .center').innerHTML = page.data.title;
+    }
+  });
